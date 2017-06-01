099ee3b25eee33e5574397fcbf66e2d1dfbfd9af
Sami <sabik.sami@gmail.com>
2017-06-01 20:16:44 +0700

    test printer

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
 from t_printer_lib import *
-import datetime,time,os,sys
-import RPi.GPIO as GPIO
-M_ID = 0
+import sys, os
 
 if len(sys.argv) == 2:
     serialport = sys.argv[1]
 else:
     serialport = ThermalPrinter.SERIALPORT
-
 if not os.path.exists(serialport):
     sys.exit("ERROR: Serial port not found at: %s" % serialport)
-
+print "Testing printer on port %s" % serialport
 p = ThermalPrinter(serialport=serialport)
-p.inverse()
-p.justify("C")
+
+M_ID = 0
+#p.inverse()
+#p.justify("C")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-os.system('clear')
 
 while True:
     input_state = GPIO.input(3)
